eeda79b0c386f99b7960b164f0986b20141fa991
Przemek Kaleta <przemyslawkaleta@gmail.com>
2021-08-18 00:06:49 +0200

    all tasks move at DOM done

--- 1_Zadania/3_Day_B/js/zadanie03.js
+++ 1_Zadania/3_Day_B/js/zadanie03.js
@@ -1,10 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     var allA = document.querySelectorAll("a");
-    for (var i =0; i < allA.length ; i++) {
-        allA[i].addEventListener("click", function () {
-            var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-            this.parentElement.style.backgroundColor = randomColor;
-        });
+
+    for(var i = 0 ; i < allA.length; i++){
+        allA[i].addEventListener("click", function (evt) {
+
+            colorBackgounfRandom(this.parentElement);
+
+        })
+
     }
+
+    var colorBackgounfRandom = function (element) {
+
+        element.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+
+    }
+
 
 });
--- 1_Zadania/3_Day_B/js/zadanie04.js
+++ 1_Zadania/3_Day_B/js/zadanie04.js
@@ -1,17 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var firstElement = document.querySelector(".first");
-    var firstChild = firstElement.firstElementChild;
-    var thirdChild = firstChild.children[2];
 
-    console.log(firstElement);
-    console.log(firstChild);
-    console.log(thirdChild);
+    var firstEl = document.querySelector(".first");
+    var secondEl = document.querySelector("#second");
+    var thirdEl = document.querySelector('h1[data-ex="third"]');
+    var fourthEl = document.querySelector(".fourth");
 
-    var secondId = document.querySelector("#second");
-    var parentId = secondId.parentElement;
-    var fourthChild = parentId.children[3];
+    console.log(firstEl);
+    console.log(secondEl);
+    console.log(thirdEl);
+    console.log(fourthEl);
 
-    //var attibuteData = document.querySelector("[data]");
+    var firstAnswer = firstEl.firstElementChild.children[2];
+    var secondAnswer = secondEl.parentElement.children[3];
 
+    var halfThirdAnswer = thirdEl.parentElement.parentElement.lastElementChild.firstElementChild.children;
+    var numb = Math.ceil(halfThirdAnswer.length / 2);
+    console.log(numb);
+    var thirdAnswer = thirdEl.parentElement.parentElement.lastElementChild.firstElementChild.children[numb - 1];
 
+    var step1fourthAnswer = fourthEl.parentElement.querySelectorAll("article");
+    var step2fourthAnswer = step1fourthAnswer[0].querySelectorAll("p");
+    var fourthAnswer = step2fourthAnswer[1];
+
+    console.log("answers");
+    console.log(firstAnswer);
+    console.log(secondAnswer);
+    console.log(thirdAnswer);
+    console.log(fourthAnswer);
 });
