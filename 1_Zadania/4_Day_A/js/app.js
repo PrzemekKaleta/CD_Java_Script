aaf06c09aabd6e05625db59dc65dde0a18a4964e
Przemek Kaleta <przemyslawkaleta@gmail.com>
2021-08-29 23:37:40 +0200

    next Day4

---
+++ 1_Zadania/4_Day_A/js/app.js
@@ -0,0 +1,12 @@
+$(function() {
+
+var allLi = $('.main li');
+allLi.addClass("borderClass colorText backgroundElement").fadeOut('slow').fadeIn('slow');
+
+var elShowMore = $('.showMore');
+elShowMore.eq(0).css('color','red');
+
+
+
+
+});
