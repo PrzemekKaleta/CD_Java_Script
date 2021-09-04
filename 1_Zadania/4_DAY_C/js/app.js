0b51353f042a6bfc12959f2ebc42553022a7aa64
Przemek Kaleta <przemyslawkaleta@gmail.com>
2021-09-04 23:55:41 +0200

    added new task with modification DOM with jquery adn events

--- 1_Zadania/4_DAY_C/js/app.js
+++ 1_Zadania/4_DAY_C/js/app.js
@@ -23,6 +23,69 @@ $(function(){
         allBts.off("click");
 
     })
+//3
+
+    var allDd = $('.superhero-description').find('dd');
+    var allDt = $('.superhero-description').find('dt');
+
+    allDd.hide();
+
+    allDt.on('click', function () {
+
+        var dd = $(this).next();
+
+        if(dd.is(':visible')){
+            dd.hide();
+        }else{
+            dd.show();
+        }
+
+
+    });
+
+//4
+    var button = $(".show-hide-btn");
+    button.type = "password";
+
+    button.on('click', function (event) {
+
+        var input = $(this).prev();
+
+        if(input.attr("type")=="password"){
+            input.attr("type","text");
+        }else if(input.attr("type")=="text")
+            input.attr("type","password");
+
+        event.preventDefault()
+    });
+
+//5
+    var menu = $('.menu');
+    menu.on('mouseenter',function () {
+        console.log("Hurrey");
+    });
+
+//6
+    var loginForm = $('form.login');
+
+    $(loginForm.find("input")).on("click",function () {
+
+        $(this).css('box-shadow','green 5px 5px 10px');
+
+    });
+
+    $(loginForm.find("input")).on("mouseleave",function () {
+
+        $(this).css('background-color','grey');
+
+    });
+
+    $(loginForm.find("input")).on("mouseenter",function () {
+
+        $(this).css('background-color','white');
+
+    });
+
 
 
 });
---
+++ 1_Zadania/4_Day_D/js/app.js
@@ -0,0 +1,69 @@
+$(function(){
+
+//1
+    var people = $('section.people');
+
+    var addButton = $("input[value='dodaj']");
+    var namePerson = $("#addUser");
+    var agePerson = $("#age");
+    var ulMain = $("ul.main");
+    var liToCollor = $(people).find("li");
+
+    var collorMe = function (element) {
+        var age = element.attr('data-age');
+
+        if(age < 16){
+            element.css('color','green');
+        }else if(age > 40){
+            element.css('color','brown');
+        }else{
+            element.css('color','blue');
+        }
+
+    };
+
+
+    liToCollor.each(function () {
+        collorMe($(this));
+    });
+
+
+    addButton.on('click', function () {
+        clickFunction();
+    });
+
+
+    var clickFunction = function () {
+
+        var personName = namePerson.val();
+        var personAge = agePerson.val();
+
+        var newLi = $('<li>' + personName + '</li>').attr('data-age',personAge);
+        collorMe(newLi);
+        ulMain.append(newLi);
+
+    }
+
+//2
+
+    var whereIAm = $(".where-i-am");
+    var div = $(whereIAm).find("div");
+
+    var span1 = $("<span>first append</span>");
+    var span2 = $("<span>second prepend</span>");
+    var span3 = $("<span>third before</span>");
+    var span4 = $("<span>fourth after</span>");
+
+    div.before(span3);
+    div.after(span4);
+    div.append(span1);
+    div.prepend(span2);
+
+
+
+
+
+
+
+
+});
